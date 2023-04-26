d32c1d218c79e20451a0467c01eac287f7e2b52e
tom-ramesh <tomramesh92@gmail.com>
2023-04-26 13:58:56 +0530

    changed api end points

--- src/apis.ts
+++ src/apis.ts
@@ -1,14 +1,14 @@
 import { getQueryString } from "./requests";
 
 export const getMostViewedArticles = async () => {
-  const data = await fetch(`http://localhost:3000/topNews`);
+  const data = await fetch(`http://localhost:4001/topNews`);
 
   return data.json();
 };
 
 export const searchArticles = async (searchString: string) => {
   const data = await fetch(
-    `http://localhost:3000/allNews?${getQueryString({ q: searchString })}`
+    `http://localhost:4001/allNews?${getQueryString({ q: searchString })}`
   );
 
   return data.json();
