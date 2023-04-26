e025756fb87284f88b174fc9c8dce1ef7cd4d74e
tom-ramesh <tomramesh92@gmail.com>
2023-04-26 15:37:21 +0530

    netlify functions

--- src/apis.ts
+++ src/apis.ts
@@ -1,14 +1,14 @@
 import { getQueryString } from "./requests";
 
 export const getMostViewedArticles = async () => {
-  const data = await fetch(`http://localhost:4001/topNews`);
+  const data = await fetch(`/.netlify/functions/topNews`);
 
   return data.json();
 };
 
 export const searchArticles = async (searchString: string) => {
   const data = await fetch(
-    `http://localhost:4001/allNews?${getQueryString({ q: searchString })}`
+    `http://localhost:3001/allNews?${getQueryString({ q: searchString })}`
   );
 
   return data.json();
