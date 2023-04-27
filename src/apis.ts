0af3b81a136f17252209930aee046a2a4ccd871d
tom-ramesh <tomramesh92@gmail.com>
2023-04-27 10:52:42 +0530

    added base path for .env file

--- src/apis.ts
+++ src/apis.ts
@@ -1,15 +1,15 @@
 import { getQueryString } from "./requests";
 
+const API_URL = "https://news-app-9876.glitch.me";
+
 export const getMostViewedArticles = async () => {
-  console.log(process.env.API_URL);
-  const data = await fetch(`${process.env.API_URL}/topNews`);
+  const data = await fetch(`${API_URL}/topNews`);
   return data.json();
 };
 
 export const searchArticles = async (searchString: string) => {
-  console.log(process.env.API_URL);
   const data = await fetch(
-    `${process.env.API_URL}/allNews?${getQueryString({ q: searchString })}`
+    `${API_URL}/allNews?${getQueryString({ q: searchString })}`
   );
 
   return data.json();
