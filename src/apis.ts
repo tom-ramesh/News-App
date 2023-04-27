598fb71100aa9fb450db8543d551b00a28bdcf30
tom-ramesh <tomramesh92@gmail.com>
2023-04-27 10:17:24 +0530

    env vaiable debugging

--- src/apis.ts
+++ src/apis.ts
@@ -1,12 +1,13 @@
 import { getQueryString } from "./requests";
 
 export const getMostViewedArticles = async () => {
+  console.log(process.env.API_URL);
   const data = await fetch(`${process.env.API_URL}/topNews`);
-
   return data.json();
 };
 
 export const searchArticles = async (searchString: string) => {
+  console.log(process.env.API_URL);
   const data = await fetch(
     `${process.env.API_URL}/allNews?${getQueryString({ q: searchString })}`
   );
