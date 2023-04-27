e8860b1423f4ae93f9a89ffca4ce641e59639716
tom-ramesh <tomramesh92@gmail.com>
2023-04-27 10:13:57 +0530

    changed env key

--- src/apis.ts
+++ src/apis.ts
@@ -1,14 +1,14 @@
 import { getQueryString } from "./requests";
 
 export const getMostViewedArticles = async () => {
-  const data = await fetch(`${process.env.API_ENDPOINT}/topNews`);
+  const data = await fetch(`${process.env.API_URL}/topNews`);
 
   return data.json();
 };
 
 export const searchArticles = async (searchString: string) => {
   const data = await fetch(
-    `${process.env.API_ENDPOINT}/allNews?${getQueryString({ q: searchString })}`
+    `${process.env.API_URL}/allNews?${getQueryString({ q: searchString })}`
   );
 
   return data.json();
